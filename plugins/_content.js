275a6bdd51f66589f706aec319da23087ad77fb7
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2025-01-08 23:38:19 +0100

    Update _content.js

--- plugins/_content.js
+++ plugins/_content.js
@@ -9,7 +9,7 @@ let handler = m => m
 handler.all = async function (m) {
 global.key = ''
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? this.user.jid : m.sender
-let pp = await this.profilePictureUrl(who, 'image').catch(_ => "https://qu.ax/boNQF.jpg")
+let pp = await this.profilePictureUrl(who, 'image').catch(_ => "https://qu.ax/bBfNO.jpg")
 
 // Fake
 global.fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
@@ -44,8 +44,8 @@ global.welgata = [tk, ig, md, yt, paypal, fb, ths, asistencia][Math.floor(Math.r
 global.redesMenu = [all, canal1, canal2, canal3, canal4, md, ig, paypal, yt, asistencia, fb, tk][Math.floor(Math.random() * 12)]
 global.accountsgb = [all, canal1, canal2, canal3, canal4, tk, ig, yt, paypal, fb, ths, md, asistencia][Math.floor(Math.random() * 13)]
 
-global.canalIdGB = ["120363160031023229@newsletter", "120363169294281316@newsletter", "120363203805910750@newsletter", "120363302472386010@newsletter"]
-global.canalNombreGB = ["INFINITY-WA 💫", "GB - UPDATE 🐈", "Tips sobre GataBot 🤩", "NEW PROJECT: YartexBot-MD ✨"]
+global.canalIdGB = ["120363258427334062@newsletter"]
+global.canalNombreGB = ["CANAL MICHIBOT😼🤖"]
 global.channelRD = await getRandomChannel()
 
 // Imágenes 
@@ -63,33 +63,33 @@ global.imagen11 = fs.readFileSync("./media/menus/img10.jpg")
 global.imagen12 = fs.readFileSync("./media/menus/img11.jpg")
 global.imagen13 = fs.readFileSync("./media/menus/img12.jpg")
 
-global.img = 'https://qu.ax/vZii.jpg'
-global.img2 = 'https://qu.ax/hjcB.jpg'
+global.img = 'https://qu.ax/bBfNO.jpg'
+global.img2 = 'https://qu.ax/bBfNO.jpg'
 
-global.img3 = 'https://qu.ax/zELg.jpg' //prem
-global.img4 = 'https://qu.ax/YueS.jpg' //prem
+global.img3 = 'https://qu.ax/bBfNO.jpg' //prem
+global.img4 = 'https://qu.ax/bBfNO.jpg' //prem
 
-global.img5 = 'https://qu.ax/xLcn.jpg'
-global.img6 = 'https://qu.ax/EGOe.jpg'
-global.img7 = 'https://qu.ax/wly.jpg'
-global.img8 = 'https://qu.ax/TPVV.jpg'
-global.img9 = 'https://qu.ax/qKlN.jpg'
+global.img5 = 'https://qu.ax/bBfNO.jpg'
+global.img6 = 'https://qu.ax/bBfNO.jpg'
+global.img7 = 'https://qu.ax/bBfNO.jpg'
+global.img8 = 'https://qu.ax/bBfNO.jpg'
+global.img9 = 'https://qu.ax/bBfNO.jpg'
 
-global.img10 = 'https://qu.ax/SYFo.jpg'
-global.img11 = 'https://qu.ax/efhn.jpg'
-global.img12 = 'https://qu.ax/Vmpl.jpg'
-global.img13 = 'https://qu.ax/ElSy.jpg'
-global.img14 = 'https://qu.ax/Tgbe.jpg'
-global.img15 = 'https://qu.ax/Puwj.jpg' 
+global.img10 = 'https://qu.ax/bBfNO.jpg'
+global.img11 = 'https://qu.ax/bBfNO.jpg'
+global.img12 = 'https://qu.ax/bBfNO.jpg'
+global.img13 = 'https://qu.ax/bBfNO.jpg'
+global.img14 = 'https://qu.ax/bBfNO.jpg'
+global.img15 = 'https://qu.ax/bBfNO.jpg' 
 
-global.img16 = 'https://qu.ax/ovkN.jpg' //+18
+global.img16 = 'https://qu.ax/bBfNO.jpg' //+18
 
-global.img17 = 'https://qu.ax/HJdr.jpg'
-global.img18 = 'https://qu.ax/HCt.jpg'
+global.img17 = 'https://qu.ax/bBfNO.jpg'
+global.img18 = 'https://qu.ax/bBfNO.jpg'
 
-global.logogit = 'https://qu.ax/RzKF.jpg'
+global.logogit = 'https://qu.ax/bBfNO.jpg'
 
-global.gataVidMenu = ['https://qu.ax/AAvg.mp4', 'https://qu.ax/SeFS.mp4', 'https://qu.ax/NqZN.mp4'][Math.floor(Math.random() * 3)]
+global.gataVidMenu = ['https://qu.ax/bBfNO.jpg', 'https://qu.ax/bBfNO.jpg', 'https://qu.ax/bBfNO.jpg'][Math.floor(Math.random() * 3)]
 global.gataMenu = [img, img2, img6, img7, img8, img9, img13, img14, img15, img17, img18][Math.floor(Math.random() * 11)]
 global.gataImg = [imagen1, imagen2, imagen3, imagen4, imagen5, imagen6, imagen7, imagen8, imagen9, imagen10, imagen11, imagen12, imagen13][Math.floor(Math.random() * 13)]
 
